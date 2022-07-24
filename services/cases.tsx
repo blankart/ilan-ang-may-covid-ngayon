1d30d2c2043a6f8607accbf3fe3f8a9e0c664721
Nico Mendoza <blankartwp@gmail.com>
2022-07-24 15:31:50 +0800

    refactor

--- services/cases.tsx
+++ services/cases.tsx
@@ -1,12 +1,13 @@
 import { load } from "cheerio";
 import dayjs from "dayjs";
 
+const URL =
+  "https://public.tableau.com/views/COVID-19CasesandDeathsinthePhilippines_15866705872710/Home?:embed=y&:showVizHome=no&:host_url=https%3A%2F%2Fpublic.tableau.com%2F&:embed_code_version=3&:tabs=no&:toolbar=yes&:animate_transition=yes&:display_static_image=no&:display_spinner=no&:display_overlay=yes&:display_count=yes&publish=yes&:loadOrderID=0";
+
 export async function getCases(date?: string) {
   try {
-    const url =
-      "https://public.tableau.com/views/COVID-19CasesandDeathsinthePhilippines_15866705872710/Home?:embed=y&:showVizHome=no&:host_url=https%3A%2F%2Fpublic.tableau.com%2F&:embed_code_version=3&:tabs=no&:toolbar=yes&:animate_transition=yes&:display_static_image=no&:display_spinner=no&:display_overlay=yes&:display_count=yes&publish=yes&:loadOrderID=0";
-    const site = await fetch(url);
-    var text = await site.text();
+    const site = await fetch(URL);
+    let text = await site.text();
     const $ = load(text);
     const tsConfigJson = JSON.parse($("#tsConfigContainer").text());
 
@@ -17,7 +18,7 @@ export async function getCases(date?: string) {
       `https://public.tableau.com${tsConfigJson.vizql_root}/bootstrapSession/sessions/${tsConfigJson.sessionid}`,
       {
         method: "POST",
-        body: body,
+        body,
       },
     );
     text = await tableauData.text();
@@ -38,7 +39,7 @@ export async function getCases(date?: string) {
 
       const [_, ACTIVE_CASE_IDX, RECOVERED_IDX, DIED_IDX] =
         _data.secondaryInfo.presModelMap.vizData.presModelHolder.genPresModelMapPresModel
-          .presModelMap["Epi_BreakdownBar"].presModelHolder.genVizDataPresModel.paneColumnsData
+          .presModelMap.Epi_BreakdownBar.presModelHolder.genVizDataPresModel.paneColumnsData
           .paneColumnsList[0].vizPaneColumns[2].aliasIndices;
 
       const [NEW_CASE_IDX] =
